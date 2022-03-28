0432c112f2216f6f55f7e818fc46032e3031b99c
Emil Tschenka Jensen <emil_jets@live.dk>
2022-03-28 12:56:17 +0200

    Tilføjet create metoder til controller

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,4 +1,27 @@
 package application.controller;
 
+import application.model.Arrangement;
+import application.model.Prisliste;
+import application.model.Produktgruppe;
+import storage.Storage;
+
 public class Controller {
+
+    public static Produktgruppe createProduktGruppe(String navn, String beskrivelse){
+        Produktgruppe produktgruppe = new Produktgruppe(navn, beskrivelse);
+        Storage.addProduktGruppe(produktgruppe);
+        return produktgruppe;
+    }
+
+    public static Prisliste createPrisliste(String navn){
+        Prisliste prisliste = new Prisliste(navn);
+        Storage.addPrisliste(prisliste);
+        return prisliste;
+    }
+
+    public static Arrangement createArrangement(String navn, String beskrivelse, double pris){
+        Arrangement arrangement = new Arrangement(navn, beskrivelse, pris);
+        Storage.addArrangement(arrangement);
+        return arrangement;
+    }
 }
